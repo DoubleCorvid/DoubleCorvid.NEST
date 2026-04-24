535d45a9d382bfd5fbc85ecc0b3fb04e6e5fcbee
doublecorvid <doublecorvid@gmail.com>
2026-04-24 11:41:59 -0400

    Reorganized Program

--- DoubleCorvid.NEST/Program.cs
+++ DoubleCorvid.NEST/Program.cs
@@ -35,71 +35,47 @@ public static class Program {
     public static void Main (string [] args) {
         _args = args;
 
-        SetupEnvironment ();
+        _cwd = Directory.GetCurrentDirectory ();
+
+        _nestSettingsPath = Path.Combine (_cwd, NESTSettingsFile);
+
+        _hostPluginPath = Path.Combine (_cwd, HostPluginFile);
+
+        _pluginsDirectory = Path.Combine (_cwd, PluginsRelativeDirectory);
+
+        _configDirectory = Path.Combine (_cwd, ConfigRelativeDirectory);
 
         _fileManager = BuildFileManager ();
 
         _settingsManager = BuildSettingsManager ();
 
-        _settingsManager.NESTSettings.ConfigDirectory = _configDirectory;
-
-        _settingsManager.NESTSettings.PluginsDirectory = _pluginsDirectory;
-
         _pluginManager = BuildPluginManager ();
 
         _nestServer = BuildNESTServer ();
-
-        _nestServer.InitilizeApp ();
         
         _nestServer.Run ();
     }
 
-    private static void SetupEnvironment () {
-        _cwd = Directory.GetCurrentDirectory ();
-
-        _nestSettingsPath = Path.Combine (_cwd, NESTSettingsFile);
-
-        // if (!File.Exists (_nestSettingsPath)) {
-        //     var json = JsonSerializer.Serialize (new NESTSettings ()) ?? throw new Exception ("Failed to serialize a default instance of NEST settings");
-
-        //     File.WriteAllText (_nestSettingsPath, json);
-        // }
-
-        _hostPluginPath = Path.Combine (_cwd, HostPluginFile);
-
-        if (!File.Exists (_hostPluginPath)) {
-            throw new Exception ($"A host plugin was not found at {_hostPluginPath}.");
-        }
-
-        _pluginsDirectory = Path.Combine (_cwd, PluginsRelativeDirectory);
-
-        if (!Directory.Exists (_pluginsDirectory)) {
-            Directory.CreateDirectory (_pluginsDirectory);
-        }
-
-        _configDirectory = Path.Combine (_cwd, ConfigRelativeDirectory);
-        
-        if (!Directory.Exists (_configDirectory)) {
-            Directory.CreateDirectory (_configDirectory);
-        }
-    }
-
     private static FileManager BuildFileManager () => new (BuildFileManagerConfig ());
 
-    private static FileManagerConfig BuildFileManagerConfig () {
-        return new () {
-            Encoding = System.Text.Encoding.UTF8
-        };
+    private static FileManagerConfig BuildFileManagerConfig () => new () {
+        Encoding = System.Text.Encoding.UTF8
+    };
+
+    private static NESTSettingsManager BuildSettingsManager () {
+        var manager = new NESTSettingsManager (BuildSettingsManagerConfig ());
+
+        manager.NESTSettings.ConfigDirectory = _configDirectory;
+
+        manager.NESTSettings.PluginsDirectory = _pluginsDirectory;
+
+        return manager;
     }
 
-    private static NESTSettingsManager BuildSettingsManager () => new (BuildSettingsManagerConfig ());
-
-    private static NESTSettingsManagerConfig BuildSettingsManagerConfig () {
-        return new NESTSettingsManagerConfig {
-            FileManager = _fileManager ?? throw new Exception ("Settings manager wasn't initilized before attempting to use it."),
-            NESTSettingsFullPath = _nestSettingsPath
-        };
-    }
+    private static NESTSettingsManagerConfig BuildSettingsManagerConfig () => new () {
+        FileManager = _fileManager ?? throw new Exception ("File manager wasn't initilized before attempting to use it."),
+        NESTSettingsFullPath = _nestSettingsPath
+    };
 
     private static PluginManager BuildPluginManager () => new (BuildPluginManagerConfig ());
 
@@ -108,7 +84,13 @@ public static class Program {
         SettingsManager = _settingsManager ?? throw new Exception ("Settings manager wasn't initilized before attempting to use it."),
     };
 
-    private static NESTServer BuildNESTServer () => new (BuildNESTServerConfig ());
+    private static NESTServer BuildNESTServer () { 
+        var server = new NESTServer (BuildNESTServerConfig ());
+
+        server.InitilizeApp ();
+
+        return server;
+    }
 
     private static NESTServerConfig BuildNESTServerConfig () => new () {
         Args = _args,
